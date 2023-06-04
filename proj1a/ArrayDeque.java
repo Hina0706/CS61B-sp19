2916a8cf44a9bac49e952ed77e9eeac117af9628
Hina <15116318341@163.com>
2023-06-03 19:42:55 -0700

    linkedlist revised

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,72 +1,68 @@
 public class ArrayDeque<T> {
 
-  private int size;
-  private int nextFirst;
-  private int nextLast;
-  private T[] a;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+    private T[] a;
 
-  public ArrayDeque() {
-    a = (T[]) new Object[8];
-    size = 0;
-    nextFirst = 4;
-    nextLast = 5;
-  }
-
-  // deep copy
-  public ArrayDeque(ArrayDeque other) {
-
-  }
-
-  private void resize() {
-
-  }
-
-  private int minusOne(int index) {
-    return 1;
-  }
-
-  private int plusOne(int index) {
-    return 1;
-  }
-
-  // add and remove must take constant time, except during resizing operations.
-  public void addFirst(T item) {
-    a[nextFirst] = item;
-    size++;
-    nextFirst = minusOne(nextFirst);
-  }
-
-  public void addLast(T item) {
-    a[nextLast] = item;
-    size++;
-    nextLast = plusOne(nextLast);
-  }
-
-  public boolean isEmpty() {
-    return size == 0;
-  }
-
-  public int size() {
-    return size;
-  }
-
-  public void printDeque() {
-    for (int i = 0; i < size; i++) {
-      System.out.print(a[i]);
+    public ArrayDeque() {
+      a = (T[]) new Object[8];
+      size = 0;
+      nextFirst = 0;
+      nextLast = 7;
     }
-    System.out.println("");
-  }
 
-  // add and remove must take constant time, except during resizing operations.
-  public T removeFirst() {
-    
-  }
+    // deep copy
+    public ArrayDeque(ArrayDeque other) {
 
-  public T removeLast() {
+    }
 
-  }
+    private int minusOne(int index) {
+      return 1;
+    }
 
-  public T get(int index) {
-    return 
-  }
+    private int plusOne(int index) {
+      return 1;
+    }
+
+    // add and remove must take constant time, except during resizing operations.
+    public void addFirst(T item) {
+      a[nextFirst] = item;
+      size++;
+      nextFirst = minusOne(nextFirst);
+    }
+
+    public void addLast(T item) {
+      a[nextLast] = item;
+      size++;
+      nextLast = plusOne(nextLast);
+    }
+
+    public boolean isEmpty() {
+      return size == 0;
+    }
+
+    public int size() {
+      return size;
+    }
+
+    public void printDeque() {
+      for (int i = 0; i < size; i++) {
+        System.out.print(a[i]);
+      }
+      System.out.println("");
+    }
+
+    // add and remove must take constant time, except during resizing operations.
+    public T removeFirst() {
+      
+    }
+
+    public T removeLast() {
+
+    }
+
+    public T get(int index) {
+        return 
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,119 +1,125 @@
 public class LinkedListDeque<T> {
 
-  private int size;
-  private IntNode sentinel;
+    private int size;
+    private IntNode sentinel;
 
-  public class IntNode {
-    T item;
-    IntNode prev;
-    IntNode next;
+    public class IntNode {
+        T item;
+        IntNode prev;
+        IntNode next;
 
-    public IntNode(T k) {
-      item = k;
-      prev = null;
-      next = null;
+        public IntNode() {
+          item = null;
+          prev = null;
+          next = null;
+        }
     }
-  }
 
-  public LinkedListDeque() {
-    size = 0;
-    sentinel = new LinkedListDeque.IntNode(0);
-    sentinel.prev = sentinel;
-    sentinel.next = sentinel;
-  }
-
-  // deep copy
-  public LinkedListDeque(LinkedListDeque other) {
-    
-  }
-
-  // add and remove operations must not involve any looping or recursion
-  // A single such operation must take “constant time”,
-  // i.e. execution time should not depend on the size of the deque.
-  public void addFirst(T item) {
-    IntNode p = sentinel.next;
-    IntNode newNode = new IntNode(item);
-    newNode.prev = sentinel;
-    newNode.next = p;
-    sentinel.next = newNode;
-    p.prev = newNode;
-    size++;
-  }
-
-  public void addLast(T item) {
-    IntNode p = sentinel.prev;
-    IntNode newNode = new IntNode(item);
-    newNode.prev = p;
-    newNode.next = sentinel;
-    p.next = newNode;
-    sentinel.prev = newNode;
-    size++;
-  }
-
-  public boolean isEmpty() {
-    return size == 0;
-  }
-
-  // size must take constant time.
-  public int size() {
-    return size;
-  }
-
-  public void printDeque() {
-    for (int i = 0; i < size; i++) {
-      System.out.print(get(i) + " ");
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new IntNode();
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
     }
-    System.out.println(" ");
-  }
 
-  // add and remove operations must not involve any looping or recursion
-  // A single such operation must take “constant time”,
-  // i.e. execution time should not depend on the size of the deque.
-  // Removes and returns the item at the front of the deque. If no such item
-  // exists, returns null.
-  public T removeFirst() {
-    if (size == 0) {
-      return null;
-    } else {
-      IntNode p = sentinel.next;
-      sentinel.next = p.next;
-      p.next.prev = sentinel;
-      size--;
-      return p.item;
+    // deep copy
+    public LinkedListDeque(LinkedListDeque other) {
+        this();
+        for (int i = 0; i < other.size; i++){
+            addLast((T) other.get(i));
+        }
     }
-  }
 
-  public T removeLast() {
-    if (size == 0) {
-      return null;
-    } else {
-      IntNode p = sentinel.prev;
-      sentinel.prev = p.prev;
-      p.prev.next = sentinel;
-      size--;
-      return p.item;
+    // add and remove operations must not involve any looping or recursion
+    // A single such operation must take “constant time”,
+    // i.e. execution time should not depend on the size of the deque.
+    public void addFirst(T item) {
+        IntNode p = sentinel.next;
+        IntNode newNode = new IntNode();
+        newNode.item = item;
+        newNode.prev = sentinel;
+        newNode.next = p;
+        sentinel.next = newNode;
+        p.prev = newNode;
+        size++;
     }
-  }
 
-  // get must use iteration, not recursion.
-  public T get(int index) {
-    IntNode p = sentinel.next;
-    while (index != 0) {
-      p = p.next;
+    public void addLast(T item) {
+        IntNode p = sentinel.prev;
+        IntNode newNode = new IntNode();
+        newNode.item = item;
+        newNode.prev = p;
+        newNode.next = sentinel;
+        p.next = newNode;
+        sentinel.prev = newNode;
+        size++;
     }
-    return p.item;
-  }
 
-  private T getRcrs(IntNode p, int index) {
-    if (index == 0) {
-      return p.item;
-    } else {
-      return getRcrs(p.next, index - 1);
+    public boolean isEmpty() {
+        return size == 0;
     }
-  }
 
-  public T getRecursive(int index) {
-    return getRcrs(sentinel.next, index);
-  }
+    // size must take constant time.
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+          System.out.print(get(i));
+        }
+        System.out.println();
+    }
+
+    // add and remove operations must not involve any looping or recursion
+    // A single such operation must take “constant time”,
+    // i.e. execution time should not depend on the size of the deque.
+    // Removes and returns the item at the front of the deque. If no such item
+    // exists, returns null.
+    public T removeFirst() {
+        if (size == 0) {
+          return null;
+        } else {
+          IntNode p = sentinel.next;
+          sentinel.next = p.next;
+          p.next.prev = sentinel;
+          size--;
+          return p.item;
+        }
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+          return null;
+        } else {
+          IntNode p = sentinel.prev;
+          sentinel.prev = p.prev;
+          p.prev.next = sentinel;
+          size--;
+          return p.item;
+        }
+    }
+
+    // get must use iteration, not recursion.
+    public T get(int index) {
+        IntNode p = sentinel.next;
+        while (index != 0) {
+          p = p.next;
+          index--;
+        }
+        return p.item;
+    }
+
+    private T getRcrs(IntNode p, int index) {
+        if (index == 0) {
+          return p.item;
+        } else {
+          return getRcrs(p.next, index - 1);
+        }
+    }
+
+    public T getRecursive(int index) {
+        return getRcrs(sentinel.next, index);
+    }
 
 }
